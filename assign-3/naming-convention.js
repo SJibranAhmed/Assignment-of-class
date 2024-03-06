97f9616d07466950dbc20967b35c107cb6f37e2c
SJibran Ahmed <Sheikhjibran889970@gmail.com>
2024-03-06 11:25:32 +0500

    naming convention

---
+++ assign-3/naming-convention.js
@@ -0,0 +1,17 @@
+//               Sheikh Jibran Ahmed               \\
+/* Naming convention: Naming cases refer to conventions for
+ naming identifiers like variables, functions, classes, or constants
+ Naming Cases:
+ 1- camelCase,
+ 2- snake_case,
+ 3- PascalCase
+ */
+// camelCase
+var candidateName = 'Sheikh Jibran Ahmed';
+console.log(candidateName + ' <-- this variable name is written in "camelCase"');
+// snake_case
+var cheif_employee_cnic = 423011595127;
+console.log(cheif_employee_cnic + ' <-- this variable name is written in "snake_case"');
+// PascalCase
+var IsPakistanAgricultural = true;
+console.log(IsPakistanAgricultural + ' <-- this variable name is written in "PascalCase"');
---
+++ assign-3/naming-convention.ts
@@ -0,0 +1,21 @@
+//               Sheikh Jibran Ahmed               \\
+  
+/* Naming convention: Naming cases refer to conventions for 
+ naming identifiers like variables, functions, classes, or constants 
+ Naming Cases:
+ 1- camelCase,
+ 2- snake_case,
+ 3- PascalCase
+ */
+
+ // camelCase
+ let candidateName:string = 'Sheikh Jibran Ahmed'
+ console.log(candidateName + ' <-- this variable name is written in "camelCase"')
+
+ // snake_case
+ let cheif_employee_cnic:number = 423011595127
+ console.log(cheif_employee_cnic + ' <-- this variable name is written in "snake_case"')
+
+ // PascalCase
+ let IsPakistanAgricultural:boolean = true
+ console.log(IsPakistanAgricultural + ' <-- this variable name is written in "PascalCase"')
